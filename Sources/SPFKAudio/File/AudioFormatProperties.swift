f89f54685675ca535efb6e65f6ba7f651d43503d
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-07-24 16:26:36 -0700

    in progress

--- Sources/SPFKAudio/File/AudioFormatProperties.swift
+++ Sources/SPFKAudio/File/AudioFormatProperties.swift
@@ -5,11 +5,38 @@ import Foundation
 
 public struct AudioFormatProperties: Hashable, Codable {
     public var channelCount: AVAudioChannelCount
-    
+
     public var sampleRate: Double
-    
+
     public var bitsPerChannel: Int?
-    
+
     public var duration: TimeInterval = 0
-    
+
+    public var formatDescription: String {
+        var out = "\(Int(sampleRate)), "
+
+        if let bitsPerChannel {
+            out += bitsPerChannel > 0 ? "\(bitsPerChannel) bit " : ""
+        }
+
+        if let channelsDescription {
+            out += "\(channelsDescription)"
+        }
+
+        return out
+    }
+
+    public var channelsDescription: String? {
+        guard channelCount > 0 else { return nil }
+
+        var out = "Stereo"
+
+        if channelCount == 1 {
+            out = "Mono"
+
+        } else if channelCount > 2 {
+            out = "\(channelCount) Channel"
+        }
+        return out
+    }
 }
