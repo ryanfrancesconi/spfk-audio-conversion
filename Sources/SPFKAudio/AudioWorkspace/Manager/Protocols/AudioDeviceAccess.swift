0b32a44f47cc1177fdab656c2ad3f001ff24e7ef
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-10-03 10:25:02 -0700

    refactor AudioEngineAccess + AudioDeviceAccess

--- Sources/SPFKAudio/AudioWorkspace/Manager/Protocols/AudioDeviceAccess.swift
+++ Sources/SPFKAudio/AudioWorkspace/Manager/Protocols/AudioDeviceAccess.swift
@@ -1,3 +1,4 @@
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/SPFKAudio
 
 public protocol AudioDeviceAccess: AnyObject {
     var audioDeviceAccess: (any AudioDeviceManagerModel)? { get }
