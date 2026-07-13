2dc7c976693ae72ae4c5812ca331b1bf7733de23
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-12 17:55:33 -0700

    Update dependency version minimums to current tags

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "1.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-metadata", from: "1.2.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "1.2.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.6.0"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-metadata", from: "1.4.6"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-lame", from: "1.0.0"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-utils", from: "1.0.2"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.1.0"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-utils", from: "1.6.0"),
         .package(url: "https://github.com/sbooth/sndfile-binary-xcframework", from: "0.1.2"),
         .package(url: "https://github.com/sbooth/ogg-binary-xcframework", from: "0.1.3"),
         .package(url: "https://github.com/sbooth/flac-binary-xcframework", from: "0.2.0"),
